9ce2ee8d48ba19beabf48d41a5abecbfb814498e
Amit Ran <57709369+amitRan109@users.noreply.github.com>
2020-09-09 08:56:52 -0400

    Add files via upload

---
+++ build/precache-manifest.c0a0ae39865ec632f969365acb8ce910.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff4c95184737414c2e6abcfcd607c3df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "b144ff1534c24798f18a",
+    "url": "/static/css/main.fe5545b3.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b144ff1534c24798f18a",
+    "url": "/static/js/main.d67e14cc.chunk.js"
+  },
+  {
+    "revision": "af1ee15f3b8733767315",
+    "url": "/static/js/runtime-main.8a8ac9cd.js"
+  },
+  {
+    "revision": "eaf24a48becec4ddae7eefa5e8a8aa9f",
+    "url": "/static/media/BerkshireSwash-Regular.eaf24a48.woff"
+  }
+]);
