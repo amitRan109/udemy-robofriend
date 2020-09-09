326e12383b6a79dc6b740be6b9025126a0373115
AmitRan109 <amitshalev109@gmail.com>
2020-09-09 16:46:12 +0300

    Updates

---
+++ precache-manifest.0d8fcf9f2d949a502e4803a4a0642fe0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6272f4addd298bc9acf885668585c734",
+    "url": "/udemy-robofriends/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/udemy-robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "271ab7304c961df6503c",
+    "url": "/udemy-robofriends/static/css/main.edd308d9.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/udemy-robofriends/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/udemy-robofriends/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "271ab7304c961df6503c",
+    "url": "/udemy-robofriends/static/js/main.149f251a.chunk.js"
+  },
+  {
+    "revision": "99b2cdd43806cbcd6989",
+    "url": "/udemy-robofriends/static/js/runtime-main.c5433a09.js"
+  },
+  {
+    "revision": "eaf24a48becec4ddae7eefa5e8a8aa9f",
+    "url": "/udemy-robofriends/static/media/BerkshireSwash-Regular.eaf24a48.woff"
+  }
+]);
